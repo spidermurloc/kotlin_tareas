0adcf779cff5025faf3b7184991e5e65c7a9ac6e
Teo Grossi <teohsgrossi@hotmail.com>
2023-08-14 08:30:54 +0200

    labels

--- app/src/main/java/com/example/primerapp/MainActivity3.kt
+++ app/src/main/java/com/example/primerapp/MainActivity3.kt
@@ -1,3 +1,5 @@
+@file:OptIn(ExperimentalMaterial3Api::class)
+
 package com.example.primerapp
 
 import android.os.Bundle
@@ -15,14 +17,21 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.primerapp.ui.theme.PrimerAppTheme
@@ -38,6 +47,7 @@ class MainActivity3 : ComponentActivity() {
         setContent {
             PrimerAppTheme {
                 datos_archivo(datos = datos)
+                label()
 
 
             }
@@ -95,12 +105,24 @@ fun datos_archivo(datos: List<datos_archivo>){
         }
     }
 }
+@Composable
+fun label() {
+    var text by remember {mutableStateOf(TextFieldValue(""))}
+    Column(){
+        OutlinedTextField(value = text, onValueChange = {x -> text = x},
+            label = {Text(text= "hola label")}
+        )
+    }
+}
+
 
 @Preview(showSystemUi = true)
 @Composable
 fun GreetingPreview() {
     PrimerAppTheme {
         datos_archivo(datos)
+        label()
 
     }
 }
+
